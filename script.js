30238a31356d6608d79d34d60d4ffd3f608510a7
Akshat Bamania <akshat.bamania23@gmail.com>
2024-12-27 17:10:59 +0530

    Callback Hell

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // inp.addEventListener('keydown', function(event) {
 //     if( event.code === "KeyA"){
 //         console.log('Player moved Left');
-        
+
 //     }else if(event.code === "KeyS"){
 //         console.log('Player moved Backward');
 //     }else if(event.code === "KeyD"){
@@ -24,9 +24,9 @@
 //     let inp = document.querySelector('input');
 
 //     console.log(inp.value);
-    
-    
-    
+
+
+
 // });
 
 
@@ -35,22 +35,57 @@
 
 // inp.addEventListener('input', function(){
 //     console.log(this.value);
-    
+
 // })
 
 
-function one (){
-    return 1;
-}
+// Stack Breakpoint..
 
-function two(){
-    return one()+ one(); 
-}
+// function one (){
+//     return 1;
+// }
 
-function three(){
-    let ans = two() + one();
-    console.log(ans);
+// function two(){
+//     return one()+ one();
+// }
+
+// function three(){
+//     let ans = two() + one();
+//     console.log(ans);
+
+// }
+
+// three();
+
+
+// js is a synchronus type language..
+// so to do things like timeout and setinterval cannot be done at through js as it do not wait for any process to execute so js provides 
+// that set of information to browser(which are built from langs like c++ or java are asynchrounus in nature) wait for that code's timeperiod 
+// and puts that code into callstack of js for js to execute it .
+
+
+let h1 = document.querySelector('h1');
+
+function colorChange(color,delay,nextColorChange){
+    setTimeout(()=>{
+        h1.style.color=color;
+        if(nextColorChange) nextColorChange(); 
+    },delay)
     
 }
+colorChange("red",1000,()=>{
+    colorChange("yellow",1000, ()=>{
+        colorChange("green",1000)
+        
+    })
+    })
 
-three();
+    //Above nesting of callback is called callback hell and is a great issue in industries..!!
+
+//The JavaScript code "if(nextColorChange) nextColorChange()" checks if the variable nextColorChange is defined and holds a value (it must be truthy, i.e., not null, undefined, 0, 
+// false, NaN, or an empty string). If it does, it will invoke nextColorChange() as a function
+
+
+
+
+
